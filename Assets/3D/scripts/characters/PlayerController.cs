d10ccf94b836a3996b2992e95bdf018956aa6b72
Gabrielle Lino <gabiisa.lino12@gmail.com>
2020-03-03 12:01:57 -0300

    Scripts incompleto

---
+++ Assets/3D/scripts/characters/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
