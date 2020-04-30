e2a0f40710dd44e7d3dce0b19d977269977b3247
Gabrielle Lino <gabi237lino>
2020-04-30 15:21:49 -0300

    ProjectileController part 1

--- Assets/scripts/characters/PlayerController.cs
+++ Assets/scripts/characters/PlayerController.cs
@@ -2,29 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-public class PlayerController : MonoBehaviour
-{
-    NavMeshAgent agent;
-    Camera cam;
-    public LayerMask walkableLayer;   
-    
-    void Start()
-    {
-        agent = GetComponent < NavMeshAgent > ();
-        cam = Camera.main;
-    }
 
-    
-    void Update()
+namespace NavGame.Core 
+{
+
+    public class PlayerController : MonoBehaviour 
     {
-        if (Input.GetMouseButtonDown(1))
+        NavMeshAgent agent;
+        Camera cam;
+        public LayerMask walkableLayer;
+        public GameObject prefab;
+
+        DamageableGameObject finalTarget;
+
+        void Awake () 
         {
-            Ray ray = cam.ScreenPointToRay (Input.mousePosition);
-            RaycastHit hit;
-            if(Physics.Raycast (ray,  out hit, Mathf.Infinity, walkableLayer))
+            agent = GetComponent<NavMeshAgent> ();
+            cam = Camera.main;
+
+            GameObject obj = GameObject.FindWithTag ("Finish");
+            
+            DamageableGameObject dgo = obj.GetComponent<DamageableGameObject> ();
+            GameObject Projectile = Instantiate (prefab, transform.position, Quaternion.identity) as GameObject;
+            ProjectileController controller = Projectile.GetComponent<ProjectileController> ();
+            controller.Init (dgo);
+        }
+
+        void Update () {
+            if (Input.GetMouseButtonDown (1)) 
             {
-                agent.SetDestination(hit.point);
+                Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast (ray, out hit, Mathf.Infinity, walkableLayer)) 
+                {
+                    agent.SetDestination (hit.point);
+                }
             }
         }
-    } 
+    }
 }
