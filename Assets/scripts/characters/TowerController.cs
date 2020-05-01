75aa4d9187c8801ff4f43d70f8374e80cd2a9606
Gabrielle Lino <gabi237lino>
2020-04-30 23:28:51 -0300

    TowerController

---
+++ Assets/scripts/characters/TowerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NavGame.Core;
+using UnityEngine;
+
+public class TowerController : ThrowerGameObject 
+{
+    protected override void UpdateAttack () 
+    {
+        if (enemiesToAttack.Count > 0) 
+            {
+                FaceObjectFrame (enemiesToAttack[0].gameObject.transform);
+                AttackOnCooldown (enemiesToAttack[0]);
+            }
+    }
+}
