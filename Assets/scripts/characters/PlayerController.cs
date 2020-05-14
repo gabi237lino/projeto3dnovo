2f1c0476f41f5ff34fa70e4060586aa03fee4b46
Gabrielle Lino <gabi237lino>
2020-05-14 03:44:21 -0300

    Performing actions

--- Assets/scripts/characters/PlayerController.cs
+++ Assets/scripts/characters/PlayerController.cs
@@ -2,91 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using NavGame.Core;
+using NavGame.Managers;
 
-namespace NavGame.Core 
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerController : TouchableGameObject 
 {
+    NavMeshAgent agent;
+    Camera cam;
+    public float range = 4f;
+    public LayerMask walkableLayer;
+    public LayerMask collectibleLayer;
+    CollectibleGameObject pickupTarget;
 
-    public class PlayerController : TouchableGameObject 
+    Vector3 actionPoint = Vector3.zero;
+
+    void Awake () 
     {
-        NavMeshAgent agent;
-        Camera cam;
-        public float range = 4f;
-        public LayerMask walkableLayer;
-        public LayerMask collectibleLayer;
-        CollectibleGameObject pickupTarget;
+        agent = GetComponent<NavMeshAgent> ();
+        cam = Camera.main;
+    }
 
-        Vector3 actionPoint = Vector3.zero;
+    void Update () 
+    {
+        ProcessInput ();
+        UpdateCollect ();
+        UpdateAction ();
+    }
+    void ProcessInput ()
+    {
 
-        void Awake () 
+        if (Input.GetMouseButtonDown (1)) 
         {
-            agent = GetComponent<NavMeshAgent> ();
-            cam = Camera.main;
-        }
+            LevelManager.instance.CancelAction();
+            actionPoint = Vector3.zero;
 
-        void Update () 
-        {
-            ProcessInput ();
-            UpdateCollect();
-            UpdateAction();
-        }
-        void ProcessInput () 
-        {
+            Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
 
-            if (Input.GetMouseButtonDown (1)) 
+            if (Physics.Raycast (ray, out hit, Mathf.Infinity, walkableLayer)) 
             {
-                Ray ray = cam.ScreenPointToRay (Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast (ray, out hit, Mathf.Infinity, walkableLayer)) 
-                {
-                    agent.SetDestination (hit.point);
-                }
-
-                if (Physics.Raycast (ray, out hit, Mathf.Infinity, collectibleLayer)) 
-                {
-                    pickupTarget = hit.collider.gameObject.GetComponent<CollectibleGameObject>();
-                    agent.SetDestination (hit.point);
-                } else 
-                {
-                    pickupTarget = null;
-                }
-            }
-            else if (Input.GetMouseButtonDown (0))
-            {
-                Ray ray = cam.ScreenPointToRay (Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast (ray, out hit, Mathf.Infinity, walkableLayer)) 
-                {
-                    actionPoint = hit.point;
-                    agent.SetDestination (hit.point);
-                }
+                agent.SetDestination (hit.point);
             }
 
-        }
-        void UpdateCollect()
-        {
-            if (pickupTarget != null)
+            if (Physics.Raycast (ray, out hit, Mathf.Infinity, collectibleLayer)) 
             {
-                if (IsInTouch(pickupTarget))
-                {
-                    pickupTarget.Pickup();
-                }
+                pickupTarget = hit.collider.gameObject.GetComponent<CollectibleGameObject> ();
+                agent.SetDestination (hit.point);
+            }
+            else 
+            {
+                pickupTarget = null;
+            }
+        } 
+        else if (Input.GetMouseButtonDown (0)&&LevelManager.instance.IsActionSelected()) 
+        {
+            Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast (ray, out hit, Mathf.Infinity, walkableLayer)) 
+            {
+                actionPoint = hit.point;
+                agent.SetDestination (hit.point);
             }
         }
 
-        void UpdateAction()
+        if (Input.GetKeyDown (KeyCode.Alpha1)) 
         {
-            if (actionPoint != Vector3.zero)
+            LevelManager.instance.SelectAction(0);
+        }
+        else if (Input.GetKeyDown (KeyCode.Alpha2)) 
+        {
+            LevelManager.instance.SelectAction(1);
+        }
+        else if (Input.GetKeyDown (KeyCode.Alpha3)) 
+        {
+            LevelManager.instance.SelectAction(2);
+        }
+
+    }
+    void UpdateCollect () 
+    {
+        if (pickupTarget != null) 
+        {
+            if (IsInTouch (pickupTarget)) 
             {
-                if (Vector3.Distance(transform.position, actionPoint) <= range)
-                {
-                    agent.ResetPath();
-                    actionPoint  = Vector3.zero;
-                }
+                pickupTarget.Pickup ();
+            }
+        }
+    }
+
+    void UpdateAction () 
+    {
+        if (actionPoint != Vector3.zero) 
+        {
+            if (Vector3.Distance (transform.position, actionPoint) <= range) 
+            {
+                agent.ResetPath();
+                LevelManager.instance.DoAction(actionPoint);
+                actionPoint = Vector3.zero;
             }
         }
     }
 }
-
-        
