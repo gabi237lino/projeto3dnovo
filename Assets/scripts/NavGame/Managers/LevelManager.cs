15eddd81c03f99d8c7ebf63ebff09032a642be67
Gabrielle Lino <gabi237lino>
2020-05-14 17:39:28 -0300

    showing action cooldown

--- Assets/scripts/NavGame/Managers/LevelManager.cs
+++ Assets/scripts/NavGame/Managers/LevelManager.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using NavGame.Core;
 using UnityEngine;
-using NavGame.Core; 
 
 namespace NavGame.Managers 
 {
@@ -14,19 +14,19 @@ namespace NavGame.Managers
 
         public OnActionSelectEvent onActionSelect;
         public OnActionCancelEvent onActionCancel;
-
+        public OnActionCooldownUpdateEvent onActionCooldownUpdate;
 
         protected int selectedAction = -1;
 
-        protected virtual void Awake()
+        protected virtual void Awake () 
         {
-            if(instance == null)
+            if (instance == null) 
             {
                 instance = this;
-            }
-            else
+            } 
+            else 
             {
-                Destroy(gameObject);
+                Destroy (gameObject);
             }
         }
 
@@ -35,50 +35,66 @@ namespace NavGame.Managers
             StartCoroutine (SpawnBad ());
         }
 
-        public virtual void SelectAction(int actionIndex)
+        public virtual void SelectAction (int actionIndex) 
         {
-            CancelAction();
+            CancelAction ();
             selectedAction = actionIndex;
-            if (onActionSelect != null)
+            if (onActionSelect != null) 
             {
-                onActionSelect(actionIndex);
+                onActionSelect (actionIndex);
             }
         }
 
-        public virtual void DoAction(Vector3 point)
+        public virtual void DoAction (Vector3 point) 
         {
             Instantiate(actions[selectedAction].prefab, point, Quaternion.identity);
             int index = selectedAction;
             selectedAction = -1;
-            if (onActionCancel != null)
-            {
-                onActionCancel(index);
-            }
-
+            StartCoroutine(ProcessCooldown(index));
         }
 
-        public virtual void CancelAction()
+        public virtual void CancelAction () 
         {
-            if (selectedAction != -1)
+            if (selectedAction != -1) 
             {
-               int index = selectedAction;
-               selectedAction = -1;
-               if (onActionCancel != null)
+                int index = selectedAction;
+                selectedAction = -1;
+                if (onActionCancel != null) 
                 {
-                    onActionCancel(index);
+                    onActionCancel (index);
                 }
             }
         }
 
-        public bool IsActionSelected()
+        public bool IsActionSelected () 
         {
             return selectedAction != -1;
+        }
+        
+        IEnumerator ProcessCooldown(int actionIndex)
+        {
+            Action action = actions[actionIndex];
+            action.coolDown = action.waitTime;
+            while (action.coolDown > 0f)
+            {
+                if (onActionCooldownUpdate != null)
+                {
+                    onActionCooldownUpdate(actionIndex, action.coolDown, action.waitTime);
+                }
+                yield return null;
+                action.coolDown -= Time.deltaTime;
+            }
+            action.coolDown = 0f;
+            if (onActionCooldownUpdate != null)
+            {
+                onActionCooldownUpdate(actionIndex, action.coolDown, action.waitTime);
+            }
         }
 
         protected abstract IEnumerator SpawnBad ();
 
         [Serializable]
-        public class Action
+        public class Action 
         {
             public int cost;
             public GameObject prefab;
